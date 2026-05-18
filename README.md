565374100dfc7e968240b8b6a92c1b720f7f2c1b
Accardo Cheng <accardoplasteim@gmail.com>
2026-05-18 16:12:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Interactive-Algorithm-Visualizer-Platform
+APython project where users can run algorithms step-by-step and visually see the process.
---
+++ kmp.py
@@ -0,0 +1,73 @@
+def kmp_steps(text: str, pattern: str):
+    steps = []
+    #Creating lps table
+    lps = [0]*len(pattern)
+    length = 0
+    i = 1
+    while i<len(pattern):
+        steps.append(
+            {
+                "phase": "build_lps",
+                "message": f"Compare pattern[{i}] with pattern[{length}]",
+                "i": i,
+                "j": length,
+                "lps": lps.copy()
+
+            }
+        )
+        if pattern[i] == pattern[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        elif length != 0:
+            length = lps[length-1]
+        else:
+            lps[i]=0
+            i += 1
+    # Search set up
+    i = 0
+    j = 0
+    matches = []
+    while i < len(text):
+        steps.append(
+            {
+                "phase": "search",
+                "message": f"Compare text[{i}] with pattern[{j}]",
+                "i": i,
+                "j": j,
+                "lps": lps.copy(),
+                "matches": matches.copy()
+
+            }
+        )
+        
+        # If characters match, both indexs move forward
+        if text[i] == pattern[j]:
+            i+=1
+            j+=1
+        if j == len(pattern):
+            # If fully matched pattern found, the index of the starting point is at i-j
+            matches.append(i-j)
+            steps.append(
+                {
+                    "phase": "match",
+                    "message": f"pattern found at index {i-j}",
+                    "i": i,
+                    "j": j,
+                    "lps": lps.copy(),
+                    "matches": matches.copy()
+
+                }
+            )
+            # Continue search for potiential matches
+            j = lps[j-1]
+        elif i < len(text) and text[i] != pattern[j]:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1         
+    return steps
+steps = kmp_steps("ABABDABACDABABCABAB", "ABABCABAB")
+
+for step in steps:
+    print(step["message"])
